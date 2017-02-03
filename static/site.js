ae76620bbd327a8eee7cf382451b39e95d1f69ec
Anthony <Gareraanthony@gmail.com>
2017-02-03 07:14:29 -0500

    Init Commit. Currently a WIP

---
+++ static/site.js
@@ -0,0 +1,9 @@
+jQuery( document ).ready( function( $ ) {
+
+  // Your JavaScript goes here
+    $('.grid').masonry({
+        // options...
+        itemSelector: '.grid-item',
+        columnWidth: 200
+    });
+});
