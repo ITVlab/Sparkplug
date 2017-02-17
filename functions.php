feb13ec4bccb87f5a514b875137a0532e632ee18
Anthony Garera <gareraanthony@gmail.com>
2017-02-17 15:01:26 +0000

    Building Footer

--- functions.php
+++ functions.php
@@ -40,6 +40,7 @@ class StarterSite extends TimberSite {
 		$context['stuff'] = 'I am a value set in your functions.php file';
 		$context['notes'] = 'These values are available everytime you call Timber::get_context();';
 		$context['menu'] = new TimberMenu();
+		$context['menu3'] = new TimberMenu();
 		$context['site'] = $this;
 		return $context;
 	}
